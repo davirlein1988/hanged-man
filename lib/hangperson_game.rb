6fbb8180ed4a2d3cf8dd2a675fed57ccb3df977d
lein <lein.davir@gmail.com>
2018-08-29 21:01:50 -0500

    added unit testing

---
+++ lib/hangperson_game.rb
@@ -0,0 +1,33 @@
+class HangpersonGame
+
+  # add the necessary class methods, attributes, etc. here
+  # to make the tests in spec/hangperson_game_spec.rb pass.
+
+  # Get a word from remote "random word" service
+
+  # def initialize()
+  # end
+  
+  
+  def initialize(word, guesses="", wrong_guesses="")
+    @word = word
+    @guesses = guesses
+    @wrong_guesses = wrong_guesses
+  end
+  attr_accessor(:word, :guesses,:wrong_guesses) 
+  # You can test it by running $ bundle exec irb -I. -r app.rb
+  # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
+  #  => "cooking"   <-- some random word
+  def self.get_random_word
+    require 'uri'
+    require 'net/http'
+    uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
+    Net::HTTP.new('watchout4snakes.com').start { |http|
+      return http.post(uri, "").body
+    }
+  end
+  def guess
+    
+  end
+
+end
